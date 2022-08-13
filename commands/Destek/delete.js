960399336bdc3d95cc07c875622bfe1344d668a4
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-13 19:01:43 +0300

    Deleted unnecessary class from import

--- commands/Destek/delete.js
+++ commands/Destek/delete.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed } = require('discord.js');
+const { Client, Message } = require('discord.js');
 const config = require("../../config.json");
 
 function deleteMsg(msg1, msg2) {
