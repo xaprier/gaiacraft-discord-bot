f69844933fa6636862648419e47cf0453b252859
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-13 21:46:02 +0300

    * Cleared garbage code with creating a function. * And used the function as it's supposed to be

--- commands/Destek/close.js
+++ commands/Destek/close.js
@@ -1,5 +1,6 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
 const config = require('../../config.json');
+const interaction = require("../../events/interactionCreate");
 
 function deleteMsg(msg1, msg2) {
     setTimeout(() => {
@@ -29,58 +30,9 @@ module.exports = {
             deleteMsg(sendMsg, message);
             return;
         }
-
-        var embed = new MessageEmbed()
-            .setTimestamp()
-            .setColor("ORANGE")
-            .setAuthor({ name: `| Destek`, iconURL: message.author.displayAvatarURL() })
-            .addFields(
-                {
-                    name: "Desteği biletini kapatmak istiyor musunuz?",
-                    value: `**Evet » ✅ **\n**Hayır » ❌ **`,
-                    inline: true,
-                },
-            )
-            .setFooter({ text: 'Developed by xaprier', iconURL: message.guild.members.cache.get(config.developer).avatarURL({ dynamic: true }) });
         
         try {
-            message.channel.send({ embeds: [embed] }).then(msg => {
-                msg.react("✅");
-                msg.react("❌");
-                const filter = (reaction, user) => {
-                    return ['✅', '❌'].includes(reaction.emoji.name) && user.id == message.author.id;
-                }
-
-                msg.awaitReactions({ filter, max: 1, time: 15000 }).then(collected => {
-                    collected.filter(obj => {
-                        return obj ? obj.emoji.name : "❌";
-                    })
-
-                    if (collected.first().emoji.name == '✅') {
-                        message.guild.channels.cache.find(c => c.name == "Talepler" && c.type == "GUILD_CATEGORY").permissionOverwrites.edit(message.guild.members.cache.get(message.channel.name.split("talep-").join("")), { VIEW_CHANNEL: false });
-
-                        message.channel.permissionOverwrites.set([{
-                            id: message.guild.roles.everyone,
-                            deny: 'VIEW_CHANNEL',
-                        }]);
-
-                        message.channel.setParent(message.guild.channels.cache.find(cha => cha.name == "Kapalı Talepler" && cha.type == "GUILD_CATEGORY"));
-                        message.channel.setName(`kapalı-${message.channel.name.split("talep-").join("")}`);
-
-                        message.channel.send({ content: `Bu destek talebi <@${message.author.id}> tarafından kapatılmıştır.` });
-                    } else {
-                        msg.delete();
-                        var embed2 = new MessageEmbed()
-                            .setTimestamp()
-                            .setColor("ORANGE")
-                            .setAuthor({ name: `| Destek`, iconURL: message.author.displayAvatarURL() })
-                            .setDescription("Kapatma iptal edildi")
-                            .setFooter({ text: 'Developed by xaprier', url: message.guild.members.cache.get(config.developer).avatarURL({ dynamic: true }) });
-                        
-                        message.channel.send({ embeds: [embed2] });
-                    }
-                })
-            })
+            interaction.closeCollector(message);
         } catch (e) { console.log(e); }
         
     }
