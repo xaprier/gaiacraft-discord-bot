f467702b3f20ad79fc6a685f6d04002d5e2ad588
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-13 18:54:46 +0300

    Changed directory name

--- commands/Utils/rolemessage.js
+++ commands/Utils/rolemessage.js
@@ -15,7 +15,7 @@ module.exports = {
         const recordChannel = message.guild.channels.cache.get(config.recordChannel);
         if (user.permissions.has("ADMINISTRATOR")) {
             const embed = new MessageEmbed()
-                .setAuthor("• Tepki Rol", message.guild.iconURL({ dynamic: true }))
+                .setAuthor({ name: "• Tepki Rol", iconURL: message.guild.iconURL({ dynamic: true }) })
                 .setDescription("Rol almak için lütfen mesaja tepki veriniz")
                 .setColor("ORANGE")
                 .setFooter({ text: 'Developed by xaprier', iconURL: message.guild.members.cache.get(config.developer).avatarURL() })
