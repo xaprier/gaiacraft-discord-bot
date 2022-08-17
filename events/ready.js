1e4c3a800718d4ea296a7bf290aa41e5a7fc315d
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-17 18:30:53 +0300

    * Tryied to fix bug

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,9 @@ const client = require("../index");
 
 client.on("ready", () => {
     console.log(`${client.user.tag} is ready to go!`);
-    client.user.setActivity("Yardım için gc!yardım", {type: "LISTENING"})
+    client.user.setActivity("Yardım için gc!yardım", {type: "LISTENING"});
+    // added activity caller every 5 minutes for not closing
+    setInterval(() => {
+        client.user.setActivity("Yardım için gc!yardım", {type: "LISTENING"})
+    }, 300000);
 });
