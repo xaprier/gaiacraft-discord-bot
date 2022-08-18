35c1bfadfb3838fed3b39ed4a4f28b003e4ae982
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-18 13:36:37 +0300

    * Edited a comment line

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const client = require("../index");
 client.on("ready", () => {
     console.log(`${client.user.tag} is ready to go!`);
     client.user.setActivity("Yardım için gc!yardım", {type: "LISTENING"});
-    // added activity caller every 5 minutes for not closing
+    // added activity caller every 5 minutes for keeping awake
     setInterval(() => {
         client.user.setActivity("Yardım için gc!yardım", {type: "LISTENING"})
     }, 300000);
