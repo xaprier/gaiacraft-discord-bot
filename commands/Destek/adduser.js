27de32fe524f399cdeeafed348e6102fa93e1af4
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-18 13:42:01 +0300

    * Added a control for user in guild or found again * If there is not a control like this, but can crash

--- commands/Destek/adduser.js
+++ commands/Destek/adduser.js
@@ -15,18 +15,22 @@ module.exports = {
 
         let ticketCategory = message.guild.channels.cache.find(cha => cha.name === 'Talepler' && cha.type === "GUILD_CATEGORY");
         try {
-            ticketCategory.permissionOverwrites.edit(member, {VIEW_CHANNEL: true});
             ticketCategory.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
-            await message.channel.permissionOverwrites.edit(member, {
-                VIEW_CHANNEL: true,
-                SEND_MESSAGES: true,
-                ATTACH_FILES: true,
-                READ_MESSAGE_HISTORY: true,
-                EMBED_LINKS: true,
-                ADD_REACTIONS: true,
-            });
             await message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
-            await message.reply({content: `<@${member.user.id}> üyesi destek talebinize eklendi`});
+            if (member) {
+                ticketCategory.permissionOverwrites.edit(member, {VIEW_CHANNEL: true});
+                await message.channel.permissionOverwrites.edit(member, {
+                    VIEW_CHANNEL: true,
+                    SEND_MESSAGES: true,
+                    ATTACH_FILES: true,
+                    READ_MESSAGE_HISTORY: true,
+                    EMBED_LINKS: true,
+                    ADD_REACTIONS: true,
+                });
+                await message.reply({content: `<@${member.user.id}> üyesi destek talebinize eklendi`});
+            } else {
+                await message.reply({content: `Üye bulunamadı.`});
+            }
         } catch (e) {
             console.log(e);
         }
--- commands/Destek/removeuser.js
+++ commands/Destek/removeuser.js
@@ -17,16 +17,19 @@ module.exports = {
         let ticketCategory = message.guild.channels.cache.find(cha => cha.name === 'Talepler' && cha.type === "GUILD_CATEGORY");
         try {
 
-            if (!message.guild.channels.cache.get(cha => cha.name === `talep-${mem.id}`))
-                ticketCategory.permissionOverwrites.edit(mem, {VIEW_CHANNEL: false});
-
             ticketCategory.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
-
-            await message.channel.permissionOverwrites.delete(mem);
-
             await message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
 
-            await message.reply({content: `<@${mem.user.id}> üyesi destek talebinizden çıkarıldı`});
+            if (!message.guild.channels.cache.get(cha => cha.name === `talep-${mem.id}`) && mem) {}
+                ticketCategory.permissionOverwrites.edit(mem, {VIEW_CHANNEL: false});
+
+            if (mem) {
+                await message.channel.permissionOverwrites.delete(mem);
+                await message.reply({content: `<@${mem.user.id}> üyesi destek talebinizden çıkarıldı`});
+            } else {
+                await message.reply({content: `Üye bulunamadı`});
+            }
+
         } catch (e) {
             console.log(e);
         }
