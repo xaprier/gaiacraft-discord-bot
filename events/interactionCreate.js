f5552e6abb56f6cd7b480dc76b9ffd8c476d5e00
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-24 15:10:44 +0300

    * Added control for there is log channel or not(crash fixed) * Added a feature of if Administrator or ticketAttendant try to click close-ticket-button to send a notification of closing ticket to the user of ticket

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,10 +3,13 @@
 const client = require("../index");
 const config = require("../config.json");
 const functions = require("../commands/Utils/funcs");
-const {MessageEmbed} = require("discord.js");
+const {MessageEmbed, MessageActionRow, MessageButton} = require("discord.js");
 
 client.on('interactionCreate', async (interaction) => {
     if (interaction.isButton()) {
+
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+
         const embed = new MessageEmbed()
             .setAuthor({name: "• Log", iconURL: interaction.guild.iconURL({dynamic: true})})
             .setFooter({
@@ -18,26 +21,54 @@ client.on('interactionCreate', async (interaction) => {
         const logChannel = interaction.guild.channels.cache.get(config.logChannel);
 
         if (interaction.customId === "destek-kapat") {
-            if (interaction.member.id !== interaction.channel.name.split("talep-").join("")) {
+            if (member.id !== interaction.channel.name.split("talep-").join("") && (!member.roles.cache.some(r => [config.ticketAttendant].includes(r.id)) && !member.permissions.has("ADMINISTRATOR"))) {
                 embed.setDescription(`${interaction.member.user.tag}, ${interaction.guild.members.cache.get(interaction.channel.name.split("talep-").join("")).user.tag} üyesinin destek talebini kapatmaya çalıştı`).setColor("RED");
-                logChannel.send({embeds: [embed]});
-                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`});
+                if (logChannel)
+                    logChannel.send({embeds: [embed]});
+                else
+                    console.log(`${config.logChannel} ID'li log kanalı bulunamadı`);
+                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`, ephemeral: true});
                 return;
             }
             embed.setDescription(`${interaction.member} destek talebini kapatma işlemi başlattı`).setColor("GREEN")
-            logChannel.send({embeds: [embed]});
+            if (logChannel)
+                logChannel.send({embeds: [embed]});
+            else
+                console.log(`${config.logChannel} ID'li log kanalı bulunamadı`);
+
+            // if attendant clicks the button bot is going to send a message to close ticket
+            if (member.roles.cache.some(r => [config.ticketAttendant].includes(r.id)) || member.permissions.has("ADMINISTRATOR")) {
+                embed.setDescription(`Başka sorununuz yoksa destek talebinizi kapatılacaktır`).setColor("ORANGE");
+                // noinspection JSCheckFunctionSignatures
+                const buttons = new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setCustomId("destek-onayla")
+                        .setLabel("Onayla")
+                        .setStyle("SUCCESS"),
+                    new MessageButton()
+                        .setCustomId("destek-reddet")
+                        .setLabel("Reddet")
+                        .setStyle("DANGER")
+                );
+                await interaction.channel.send({ embeds: [embed], components: [buttons] });
+                return;
+            }
 
             await functions.closeCollector(interaction);
         } else if (interaction.customId === "destek-onayla") {
-            if (interaction.member.id !== interaction.channel.name.split("talep-").join("")) {
-                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`});
+            if (member.id !== interaction.channel.name.split("talep-").join("") && (!member.roles.cache.some(r => [config.ticketAttendant].includes(r.id)) && !member.permissions.has("ADMINISTRATOR"))) {
+                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`, ephemeral: true});
                 embed.setDescription(`${interaction.member.user.tag}, ${interaction.guild.members.cache.get(interaction.channel.name.split("talep-").join("")).user.tag} üyesinin destek talebini kapatma işlemini onaylamaya çalıştı`).setColor("RED");
-                logChannel.send({embeds: [embed]});
+                if (logChannel)
+                    logChannel.send({embeds: [embed]});
+                else
+                    console.log(`${config.logChannel} ID'li log kanalı bulunamadı`);
                 return;
             }
 
             embed.setDescription(`${interaction.member} destek talebini kapatma işlemini onayladı`).setColor("GREEN")
-            logChannel.send({embeds: [embed]});
+            if (logChannel)
+                logChannel.send({embeds: [embed]});
 
             await functions.ticketSystemCreate(interaction);
 
@@ -62,9 +93,12 @@ client.on('interactionCreate', async (interaction) => {
 
         } else if (interaction.customId === "destek-reddet") {
             if (interaction.member.id !== interaction.channel.name.split("talep-").join("")) {
-                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`});
+                interaction.reply({content: `Sadece talep sahibi, talebi kapatabilir`, ephemeral: true});
                 embed.setDescription(`${interaction.member.user.tag}, ${interaction.guild.members.cache.get(interaction.channel.name.split("talep-").join("")).user.tag} üyesinin destek talebini kapatma işlemini reddetmeye çalıştı`).setColor("RED");
-                logChannel.send({embeds: [embed]});
+                if (logChannel)
+                    logChannel.send({embeds: [embed]});
+                else
+                    console.log(`${config.logChannel} ID'li log kanalı bulunamadı`);
                 return;
             }
 
@@ -78,9 +112,12 @@ client.on('interactionCreate', async (interaction) => {
                     url: interaction.guild.members.cache.get(config.developer).displayAvatarURL({dynamic: true}) || null
                 });
 
-            interaction.reply({embeds: [embed2]});
+            interaction.reply({embeds: [embed2], ephemeral: true});
             embed.setDescription(`${interaction.member} destek talebini kapatma işlemini reddetti`).setColor("GREEN")
-            logChannel.send({embeds: [embed]});
+            if (logChannel)
+                logChannel.send({embeds: [embed]});
+            else
+                console.log(`${config.logChannel} ID'li log kanalı bulunamadı`);
         } else if (interaction.customId === "destek-oluştur") {
             // controlling ticket system
             await functions.ticketSystemCreate(interaction);
