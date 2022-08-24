16c4d1f619b5a9a1275db04ce2329914f09a2129
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-24 14:21:37 +0300

    * Added additional checks and fixed crash if no parameter entered

--- commands/Destek/removeuser.js
+++ commands/Destek/removeuser.js
@@ -13,7 +13,8 @@ module.exports = {
      */
     run: async (client, message, args) => {
         let mem = await functions.checks(message, args);
-
+        // check if there is an error on checks
+        if (mem === 1) return;
         // check ticket system is correct
         await functions.ticketSystemCreate(message);
 
@@ -23,14 +24,13 @@ module.exports = {
             ticketCategory.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
             await message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {VIEW_CHANNEL: false});
 
-            if (!message.guild.channels.cache.get(cha => cha.name === `talep-${mem.id}`) && mem) {}
-                ticketCategory.permissionOverwrites.edit(mem, {VIEW_CHANNEL: false});
-
             if (mem) {
+                if (!message.guild.channels.cache.get(cha => cha.name === `talep-${mem.id}`) && mem) {}
+                    ticketCategory.permissionOverwrites.edit(mem, {VIEW_CHANNEL: false});
                 await message.channel.permissionOverwrites.delete(mem);
                 await message.reply({content: `<@${mem.user.id}> üyesi destek talebinizden çıkarıldı`});
             } else {
-                await message.reply({content: `Üye bulunamadı`});
+                await message.reply({content: `Üye bulunamadı`}).then(msg => setTimeout(() => msg.delete(), 5000));
             }
 
         } catch (e) {
