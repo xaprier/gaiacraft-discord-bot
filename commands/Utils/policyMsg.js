110fedf450777a502b97efedc677f243c4421361
Seymen Kalkan <seymenkalkan@gmail.com>
2022-09-09 13:56:16 +0300

    Added missing ? operators which will create a crash if not exists. Added some missing await calls

--- commands/Utils/policyMsg.js
+++ commands/Utils/policyMsg.js
@@ -19,85 +19,85 @@ module.exports = {
 				.setDescription( `Hesap güvenliği ve eşya kaybı sorumluluğu size aittir. GaiaCraft yalnızca kendi sebep olduğu durumların sorumlusudur.` )
 				.setFooter( {
 					text: 'Developed by xaprier',
-					iconURL: message.guild.members.cache.get( config.developer ).displayAvatarURL( {dynamic: true} )
+					iconURL: message.guild.members.cache.get( config.developer )?.displayAvatarURL( {dynamic: true} )
 				} )
 				.setColor( "ORANGE" )
 				.setTimestamp();
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Güvendiğiniz kişilerin sorumluluğu size aittir. GaiaCraft sizlere bunun için tapu/takas/müzayede gibi çeşitli sistemler sunmuştur. Herhangi bir eşya kaybında GaiaCraft sorumlu değildir.` ).author.name = `Güvenilir Sistemler`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Hesap güvenliği için GaiaCraft sunucusunda başka yerlerde kullanmadığınız şifreler kullanınız. 123456, kullanıcı adınız gibi çeşitli şifreler belirlenen hesaplar kolay şekilde çalınabilir. Hesap çalmalara karşın oto-ban sistemimiz bulunmaktadır. Bu yüzden şifrelerinizi bir yere kaydediniz. Yoksa şifre unutma durumunda hesabınızı kurtaramayabiliriz.` ).author.name = `Hesap Güvenliği`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft sunucusu ticari kullanım amaçlı sunulan bir hizmet değildir. Bu yüzden oyun içi eşyalarınızı, gerçek parayla oyuncuya satmak kalıcı ban sebebidir.` ).author.name = `Ticari Kullanım`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft gerekli durumlarda hesabınıza el koyabilir, silebilir, düzenleyebilir veya kalıcı yasaklayabilir.` ).author.name = `El Koyma Hakkı`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft sunucusuna yaptığınız bağışların geri dönüşü yoktur. Yaptığınız her bağış sunucuya katkıda bulunup aynı zamanda oyunda dilediğiniz ürünü alabilmenizi sağlar. Ürünler sadece GaiaCraft'ın kendi sitesinde satılıp, başka yerde satışı kalıcı yasaklanır.` ).author.name = `Yapılan Bağışlar`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft ekibini oyalayıcı yönde destek talebinde bulunmak 7 gün yasaklanır. Destek ekibi aynı zamanda hile kontrol gibi gerekli görüldüğü durumlarda Alpemix/TeamViewer gibi uygulamalarla bilgisayarınıza erişim sağlayıp kişisel verilerinize dokunmadan kontrol yapma yetkisine sahiptir. Bu kontrolü reddetmek 7 gün yasaklanır.` ).author.name = `Destek Ekibi`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft'ta oynayan her oyuncu bu kuralları ve hizmet politikasını kabul etmiş sayılır. İşlem yapılırken bunları okuduğunuz göz önüne alınır.` ).author.name = `Hizmet Şartları ve Gizlilik Politikası`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft gerekli gördüğü durumlarda bu politikayı ve hizmet koşullarını değiştirme hakkına sahiptir.` ).author.name = `Değiştirme Hakkı`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
+				await policyChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `GaiaCraft yetkili ekibinden olan insanların yetkilerini kötüye kullanımı sonucu yetkisi alınıp sunucudan 7 gün yasaklanır.` ).author.name = `Yetkiyi Kötüye Kullanım`;
 			try {
-				await policyChannel.send( {embeds: [embed]} );
-				message.delete();
+				await policyChannel?.send( {embeds: [embed]} );
+				await message?.delete();
 			} catch ( e ) {
 				console.log( e );
 			}
 
 		} else {
-			await message.channel.send( {content: `Politikamız <#${config.policyChannel.id}> kanalındadır.`} ).then( cha => {
+			await message.channel?.send( {content: `Politikamız <#${policyChannel.id}> kanalındadır.`} ).then( cha => {
 				setTimeout( () => {
-					cha.delete();
-					message.delete();
+					cha?.delete();
+					message?.delete();
 				}, 15000 );
 			} ).catch( ( e ) => console.log( e ) );
 		}
--- commands/Utils/rulesmsg.js
+++ commands/Utils/rulesmsg.js
@@ -22,46 +22,46 @@ module.exports = {
 				.setColor( "ORANGE" )
 				.setFooter( {
 					text: 'Developed by xaprier',
-					iconURL: message.guild.members.cache.get( config.developer ).displayAvatarURL( {dynamic: true} )
+					iconURL: message.guild.members.cache.get( config.developer )?.displayAvatarURL( {dynamic: true} )
 				} )
 				.setTimestamp();
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Sunucuyu laga/çökmeye vb. olaylara sebep olacak sistemler geri alınmaksızın silinir. Ceza hakkında bilgi almak için <#${config.punishChannel}> kanalını görüntüleyebilirsiniz.` ).author.name = `Bozuk Sistemler`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Lavacast yapmak yasaktır. Ceza hakkında bilgi almak için <#${config.punishChannel}> kanalını görüntüleyebilirsiniz.` ).author.name = `Lavacast`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Birisinin tapusunun etrafına tapu atmak, tapusunun etrafına sur çekmek, eşik kazmak, lav veya su koymak Tapu Troll'e girer ve cezalandırılır. Ceza hakkında bilgi almak için <#${config.punishChannel}> kanalını görüntüleyebilirsiniz.` ).author.name = `Tapu Trolleme`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Tapusuz alana tuzak yapmak, çukur kazmak yasaktır. Ceza hakkında bilgi almak için <#${config.punishChannel}> kanalını görüntüleyebilirsiniz.` ).author.name = `Tuzak Sistemleri`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
 
 			embed.setDescription( `Sorunlarınızı genel-sohbet, galeri, ve diğer herkese açık metin kanalları, yetkiliye özelden ulaşmak yerine <#${config.onlyCommands}> kanalına **gc!destek** yazıp destek talebi açarak sorunlarınızı belirtiniz. Ceza hakkında bilgi almak için <#${config.punishChannel}> kanalını görüntüleyebilirsiniz.` ).author.name = `Sorunlar`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
+				await ruleChannel?.send( {embeds: [embed]} );
 			} catch ( e ) {
 				console.log( e );
 			}
@@ -82,17 +82,17 @@ module.exports = {
 				}
 			).author.name = `Discord`;
 			try {
-				await ruleChannel.send( {embeds: [embed]} );
-				await message.delete();
+				await ruleChannel?.send( {embeds: [embed]} );
+				await message?.delete();
 			} catch ( e ) {
 				console.log( e );
 			}
 
 		} else {
-			await message.channel.send( {content: `Kurallarımız <#${config.rulesChannel.id}> kanalındadır.`} ).then( cha => {
+			await message.channel?.send( {content: `Kurallarımız <#${ruleChannel.id}> kanalındadır.`} ).then( cha => {
 				setTimeout( () => {
-					cha.delete();
-					message.delete();
+					cha?.delete();
+					message?.delete();
 				}, 15000 );
 			} ).catch( ( e ) => console.log( e ) );
 		}
